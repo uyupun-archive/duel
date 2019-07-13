22cd847d3be07c92dc694e3e46818714ecafab69
takashi <takashioicsn@gmail.com>
2019-07-14 05:55:51 +0900

    modify complete

--- pages/complete/index.vue
+++ pages/complete/index.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="mt-4">
+  <div class="section">
     <h2 class="h2">和解成立！</h2>
     <router-link to="/">
-      <button type="button" class="btn btn-primary">トップへ</button>
+      <button type="button" class="btn btn-primary btn-position">トップへ</button>
     </router-link>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.section {
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
+
+.h2 {
+  padding-top: 50px;
+}
+
+.btn-position {
+  display: inline-block;
+  position: absolute;
+  bottom: 100px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
