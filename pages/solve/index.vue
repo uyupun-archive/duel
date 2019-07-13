e2a380854eefbd20d6ae3b90549d30eaba7a6fbb
takashi <takashioicsn@gmail.com>
2019-07-14 07:17:03 +0900

    modify solve

--- pages/solve/index.vue
+++ pages/solve/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="section">
     <div class="c-h20">
-      <button type="button" class="btn btn-primary c-translateY125 d-inline-block" @click="makePeace('A')">和解する</button>
-      <button type="button" class="btn btn-danger" @click="notMakePeace('A')">和解しない</button>
+      <button type="button" class="btn btn-danger c-rotateZ180 d-inline-block" @click="notMakePeace('A')">和解しない</button>
+      <button type="button" class="btn btn-primary c-rotateZ180 d-inline-block" @click="makePeace('A')">和解する</button>
     </div>
-    <div class="c-h60">
-      <p class="c-rotateZ90 c-font30">和解しますか？</p>
-    </div>
-    <div class="c-h20">
+    <div class="c-playerA">プレイヤー: {{ $store.state.playerA }}</div>
+    <p class="c-rotateZ90 c-text">和解しますか？</p>
+    <div class="c-playerB">プレイヤー: {{ $store.state.playerB }}</div>
+    <div class="c-b20">
       <button type="button" class="btn btn-primary c-translateY125 d-inline-block" @click="makePeace('B')">和解する</button>
-      <button type="button" class="btn btn-danger" @click="notMakePeace('B')">和解しない</button>
+      <button type="button" class="btn btn-danger c-translateY125 d-inline-block" @click="notMakePeace('B')">和解しない</button>
     </div>
   </div>
 </template>
@@ -67,10 +67,8 @@ export default {
 <style lang="scss" scoped>
 .section {
   overflow: hidden;
-}
-
-.c-font30 {
-  font-size: 30px;
+  height: 100vh;
+  position: relative;
 }
 
 .c-rotateZ {
@@ -78,8 +76,29 @@ export default {
     transform: rotateZ(180deg) translateY(-125%);
   }
   &90 {
-    transform: rotateZ(90deg) translateX(50%);
+    transform: rotateZ(90deg);
   }
+}
+
+.c-playerA {
+  position: absolute;
+  top: 16vh;
+  left: 50%;
+  transform: rotate(180deg) translateX(50%);
+}
+
+.c-playerB {
+  position: absolute;
+  bottom: 16vh;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.c-text {
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  font-size: 30px;
 }
 
 .c-translateY125 {
@@ -93,5 +112,14 @@ export default {
   &20 {
     height: 20vh;
   }
+}
+
+.c-b20 {
+  height: 20vh;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
