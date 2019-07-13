66fb12ab75a0aa118b1be82a0cef60cd091058a2
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-14 04:47:45 +0900

    Modify complete

--- pages/complete/index.vue
+++ pages/complete/index.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="mt-4">
-    <h2>和解成立！</h2>
-    <div>
-      <router-link to="/">
-        <button type="button" class="btn btn-danger">トップへ戻る</button>
-      </router-link>
-    </div>
+    <h2 class="h2">和解成立！</h2>
+    <router-link to="/">
+      <button type="button" class="btn btn-primary">トップへ</button>
+    </router-link>
   </div>
 </template>
