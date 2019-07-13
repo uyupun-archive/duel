e85f6bd070cc2ee1558c372c4bffa91015afdc0d
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-14 04:44:48 +0900

    Modify duel

--- pages/duel/index.vue
+++ pages/duel/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <div class="mt-4 reverse">
+    <div class="reverse">
       <p>2:00</p>
       <router-link to="/solve">
         <button class="btn btn-primary">話し終わる</button>
       </router-link>
     </div>
-    <div class="cards"></div>
-    <div class="mt-4">
+
+    <div class="deck mt-4 mb-4"></div>
+
+    <div>
       <p>2:00</p>
       <router-link to="/solve">
         <button class="btn btn-primary">話し終わる</button>
@@ -16,10 +18,15 @@
   </div>
 </template>
 <style>
-.cards {
-  width: 60%;
-  height: 300px;
-  border-style:solid; 
+.deck {
+  cursor: pointer;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 300px;
+  height: 450px;
+  border-radius: 5px;
+  border: 8px solid #333333;
+  background: repeating-linear-gradient(45deg, #dc143c 0px, #dc143c 15px, #fffafa 15px, #fffafa 30px);
 }
 .reverse {
   transform:rotate(180deg);
