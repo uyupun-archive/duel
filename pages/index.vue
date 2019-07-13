7fb32d6421f0b3af06ee1d1a851a49733e44ec05
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-14 04:34:00 +0900

    Modify top

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
     <h1 class="h1">Duel</h1>
-    <input type="text" placeholder="争いの種" class="form-control" />
-    <router-link to="/reason">
-      <button type="button" class="btn btn-primary">スタンバイ</button>
+    <input type="text" placeholder="争いの種" class="form-control" v-model="fightName" />
+    <router-link to="/duel">
+      <button type="button" class="btn btn-primary" :disabled="validateFightName()">スタンバイ</button>
     </router-link>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      fightName: '',
+    }
+  },
+  methods: {
+    validateFightName() {
+      if (this.fightName.length >= 5 && this.fightName.length <= 20) return false;
+      return true;
+    }
+  }
+}
+</script>
--- pages/reason/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    reasonページ
-    <router-link to="/duel">duelへ</router-link>
-  </div>
-</template>
