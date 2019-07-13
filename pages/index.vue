9f7dd61f9ef42335dcd78607b13fd28e0cabfe18
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-14 03:57:59 +0900

    Modify top

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="mt-4">
-    <h1>Duel</h1>
-    <input type="text" value="争いの種" />
-    <br />
+  <div>
+    <h1 class="h1">Duel</h1>
+    <input type="text" placeholder="争いの種" class="form-control" />
     <router-link to="/reason">
       <button type="button" class="btn btn-primary">スタンバイ</button>
     </router-link>
