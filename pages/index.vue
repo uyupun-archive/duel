59cab0defb6bccc1867f79642c9291b58269572c
scion.tanaka <gamelemon_317@yahoo.co.jp>
2019-07-14 03:47:07 +0900

    modify index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    topページ
-    <router-link to="/reason">reasonへ</router-link>
+  <div class="mt-4">
+    <h1>Duel</h1>
+    <input type="text" value="争いの種" />
+    <br />
+    <router-link to="/reason">
+      <button type="button" class="btn btn-primary">スタンバイ</button>
+    </router-link>
   </div>
 </template>
